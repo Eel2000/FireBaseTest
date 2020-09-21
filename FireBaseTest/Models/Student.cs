34edb59b9679a8badf25520567a5472ded7f485d
Eel2000 <eliezerbwana@gmaill.com>
2020-09-21 12:24:01 +0200

    ✨ Add Student Model

---
+++ FireBaseTest/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireBaseTest.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Grade { get; set; }
+        public string OriginSchool { get; set; }
+        public string Age { get; set; }
+    }
+}
